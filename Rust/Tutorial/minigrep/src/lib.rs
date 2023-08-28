0d4bb7fbb22cf371211ec0a84598995ac5ca2124
Shashank P <shashankp2832@gmail.com>
2023-08-28 20:44:47 +0530

    Added iterators to minigrep

--- Rust/Tutorial/minigrep/src/lib.rs
+++ Rust/Tutorial/minigrep/src/lib.rs
@@ -10,15 +10,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() != 3 {
-            return Err("Usage: exec <query> <filename>")
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        
+        let query = match args.next() {
+            Some(x) => x,
+            None => return Err("Did not get a query string!")
+        };
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let filename = match args.next() {
+            Some(x) => x,
+            None => return Err("Did not get filename!")
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-
         Ok(Config{query, filename, case_sensitive})
     }
 }
@@ -38,24 +43,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+    .lines()
+    .filter(|x| x.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|x| x.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 
--- Rust/Tutorial/minigrep/src/main.rs
+++ Rust/Tutorial/minigrep/src/main.rs
@@ -5,12 +5,11 @@ use minigrep::Config;
   
 fn main() {
     
-    let args: Vec<String> = env::args().collect();
     let err_handler = | err: &str | {
         eprintln!("Problem Parsing Arguments: {}", err);
         process::exit(1);
     };
-    let config = Config::new(&args).unwrap_or_else(err_handler);
+    let config = Config::new(env::args()).unwrap_or_else(err_handler);
 
     println!("Searching: {}", config.query);
     println!("File: {}", config.filename);
