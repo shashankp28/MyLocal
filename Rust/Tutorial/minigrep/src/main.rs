cd12ee64ab2061c036a984845fdcd3b05023d5e0
Shashank P <shashankp2832@gmail.com>
2023-08-28 16:50:50 +0530

    Completed Basic CLI App

--- Rust/Tutorial/minigrep/src/main.rs
+++ Rust/Tutorial/minigrep/src/main.rs
@@ -1,3 +1,22 @@
+use std::process;
+use std::env;
+use minigrep::Config;
+
+  
 fn main() {
-    println!("Hello, world!");
+    
+    let args: Vec<String> = env::args().collect();
+    let err_handler = | err: &str | {
+        eprintln!("Problem Parsing Arguments: {}", err);
+        process::exit(1);
+    };
+    let config = Config::new(&args).unwrap_or_else(err_handler);
+
+    println!("Searching: {}", config.query);
+    println!("File: {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
 }
