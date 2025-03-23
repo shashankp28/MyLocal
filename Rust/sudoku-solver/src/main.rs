a139bdc30813d4a7bf500e6d3b05d9eb4ca6c2eb
Shashank P <shashankp2832@gmail.com>
2025-03-23 18:01:17 +0530

    Pushed smaller projects into this reporsitory

---
+++ Rust/sudoku-solver/src/main.rs
@@ -0,0 +1,24 @@
+use sudoku_solver::def::Sudoku;
+fn main() {
+    let board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    let mut sudoku = Sudoku::new(board);
+    sudoku.print_board();
+    let solved = sudoku.solve();
+    if solved {
+        println!("Board is solved!\n");
+    } else {
+        println!("Board could not be solved\n");
+    }
+    sudoku.print_board();
+}
